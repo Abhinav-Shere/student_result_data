eed77dfd82830e5a3c085064ec993ddd1643bfde
Abhinav-Shere <abhhinavdev12@gmail.com>
2023-07-27 18:20:55 +0530

    code optimize

--- app/controllers/daily_result_stats_controller.rb
+++ app/controllers/daily_result_stats_controller.rb
@@ -1,24 +1,22 @@
 class DailyResultStatsController < ApplicationController
 	def create
-    begin
-      subjects = ResultDatum.distinct.pluck(:subject)
+    subjects = ResultDatum.distinct.pluck(:subject)
 
-      subjects.each do |subject|
-        result_data_per_day = ResultDatum.where(subject: subject).group_by { |data| data.timestamp.to_date }
+    subjects.each do |subject|
+      result_data_per_day = ResultDatum.where(subject: subject).group_by { |data| data.timestamp.to_date }
 
-        result_data_per_day.each do |date, data|
-          daily_low = data.min_by(&:marks).marks
-          daily_high = data.max_by(&:marks).marks
-          result_count = data.size
-          
-          DailyResultStat.create!(
-            date: date,
-            subject: subject,
-            daily_low: daily_low,
-            daily_high: daily_high,
-            result_count: result_count
-          )
-        end
+      result_data_per_day.each do |date, data|
+        daily_low = data.min_by(&:marks).marks
+        daily_high = data.max_by(&:marks).marks
+        result_count = data.size
+        
+        DailyResultStat.create!(
+          date: date,
+          subject: subject,
+          daily_low: daily_low,
+          daily_high: daily_high,
+          result_count: result_count
+        )
       end
     end
   end
--- app/controllers/monthly_averages_controller.rb
+++ app/controllers/monthly_averages_controller.rb
@@ -1,24 +1,22 @@
 class MonthlyAveragesController < ApplicationController
 	def create
-    begin
-      subjects = ResultDatum.distinct.pluck(:subject)
+    subjects = ResultDatum.distinct.pluck(:subject)
 
-      subjects.each do |subject|
-        result_data_per_month = ResultDatum.where(subject: subject).group_by { |data| data.timestamp.beginning_of_month }
+    subjects.each do |subject|
+      result_data_per_month = ResultDatum.where(subject: subject).group_by { |data| data.timestamp.beginning_of_month }
 
-        result_data_per_month.each do |month, data|
-          monthly_avg_low = data.map(&:marks).sum / data.size
-          monthly_avg_high = data.map(&:marks).max
-          result_count_used = data.size
+      result_data_per_month.each do |month, data|
+        monthly_avg_low = data.map(&:marks).sum / data.size
+        monthly_avg_high = data.map(&:marks).max
+        result_count_used = data.size
 
-          MonthlyAverage.create!(
-            date: month,
-            subject: subject,
-            monthly_avg_low: monthly_avg_low,
-            monthly_avg_high: monthly_avg_high,
-            monthly_result_count_used: result_count_used
-          )
-        end
+        MonthlyAverage.create!(
+          date: month,
+          subject: subject,
+          monthly_avg_low: monthly_avg_low,
+          monthly_avg_high: monthly_avg_high,
+          monthly_result_count_used: result_count_used
+        )
       end
     end
   end
