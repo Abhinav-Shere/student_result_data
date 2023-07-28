308047f5330369f060f575888a6fe664e052669a
Abhinav-Shere <abhhinavdev12@gmail.com>
2023-07-28 14:38:58 +0530

    Add benchmark gem

--- app/controllers/monthly_averages_controller.rb
+++ app/controllers/monthly_averages_controller.rb
@@ -1,11 +1,15 @@
 class MonthlyAveragesController < ApplicationController
   def create
-    subjects = ResultDatum.distinct.pluck(:subject)
+    result = Benchmark.measure do
+      subjects = ResultDatum.distinct.pluck(:subject)
 
-    subjects.each do |subject|
-      daily_result_processing_service = MonthlyResultProcessingService.new(subject)
-      daily_result_processing_service.process_and_save_stats
+      subjects.each do |subject|
+        daily_result_processing_service = MonthlyResultProcessingService.new(subject)
+        daily_result_processing_service.process_and_save_stats
+      end
     end
+    
+    puts "Execution time for create action: #{result.total} seconds"
   end
 
   def index
